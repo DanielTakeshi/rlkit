db81885b68dda60c93b89c645db2bfe739344ad8
DanielTakeshi <takeshidanny@gmail.com>
2019-06-20 12:46:54 -0700

    adding ddpg script extension to rlkit

--- examples/ddpg_more_envs.py
+++ examples/ddpg_more_envs.py
@@ -1,11 +1,5 @@
 """
-This should results in an average return of ~3000 by the end of training.
-
-Usually hits 3000 around epoch 80-100. Within a see, the performance will be
-a bit noisy from one epoch to the next (occasionally dips dow to ~2000).
-
-Note that one epoch = 5k steps, so 200 epochs = 1 million steps.
-
+Example of running PyTorch implementation of DDPG on HalfCheetah.
 Daniel: changing to support other envs.
 """
 from gym.envs.mujoco import (
@@ -16,25 +10,26 @@ from gym.envs.mujoco import (
         Walker2dEnv,
 )
 import argparse
+import copy
 
-import rlkit.torch.pytorch_util as ptu
 from rlkit.data_management.env_replay_buffer import EnvReplayBuffer
 from rlkit.envs.wrappers import NormalizedBoxEnv
-from rlkit.exploration_strategies.base import \
+from rlkit.exploration_strategies.base import (
     PolicyWrappedWithExplorationStrategy
-from rlkit.exploration_strategies.gaussian_strategy import GaussianStrategy
+)
+from rlkit.exploration_strategies.ou_strategy import OUStrategy
 from rlkit.launchers.launcher_util import setup_logger
 from rlkit.samplers.data_collector import MdpPathCollector
 from rlkit.torch.networks import FlattenMlp, TanhMlpPolicy
-from rlkit.torch.td3.td3 import TD3Trainer
+from rlkit.torch.ddpg.ddpg import DDPGTrainer
+import rlkit.torch.pytorch_util as ptu
 from rlkit.torch.torch_rl_algorithm import TorchBatchRLAlgorithm
 
 
-def experiment(variant, args):
-    # Doesn't work :(
-    #import gym
-    #expl_env = NormalizedBoxEnv( gym.make(args.env) )
-    #eval_env = NormalizedBoxEnv( gym.make(args.env) )
+def experiment(variant):
+    # Or for a specific version (Daniel: doesn't work):
+    # import gym
+    # env = NormalizedBoxEnv(gym.make('HalfCheetah-v1'))
 
     if 'Ant' in args.env:
         expl_env = NormalizedBoxEnv( AntEnv() )
@@ -54,25 +49,9 @@ def experiment(variant, args):
     else:
         raise ValueError(args.env)
 
-    # Back to normal.
-    obs_dim = expl_env.observation_space.low.size
-    action_dim = expl_env.action_space.low.size
-    qf1 = FlattenMlp(
-        input_size=obs_dim + action_dim,
-        output_size=1,
-        **variant['qf_kwargs']
-    )
-    qf2 = FlattenMlp(
-        input_size=obs_dim + action_dim,
-        output_size=1,
-        **variant['qf_kwargs']
-    )
-    target_qf1 = FlattenMlp(
-        input_size=obs_dim + action_dim,
-        output_size=1,
-        **variant['qf_kwargs']
-    )
-    target_qf2 = FlattenMlp(
+    obs_dim = eval_env.observation_space.low.size
+    action_dim = eval_env.action_space.low.size
+    qf = FlattenMlp(
         input_size=obs_dim + action_dim,
         output_size=1,
         **variant['qf_kwargs']
@@ -82,38 +61,19 @@ def experiment(variant, args):
         output_size=action_dim,
         **variant['policy_kwargs']
     )
-    target_policy = TanhMlpPolicy(
-        input_size=obs_dim,
-        output_size=action_dim,
-        **variant['policy_kwargs']
-    )
-    es = GaussianStrategy(
-        action_space=expl_env.action_space,
-        max_sigma=0.1,
-        min_sigma=0.1,  # Constant sigma
-    )
+    target_qf = copy.deepcopy(qf)
+    target_policy = copy.deepcopy(policy)
+    eval_path_collector = MdpPathCollector(eval_env, policy)
     exploration_policy = PolicyWrappedWithExplorationStrategy(
-        exploration_strategy=es,
+        exploration_strategy=OUStrategy(action_space=expl_env.action_space),
         policy=policy,
     )
-    eval_path_collector = MdpPathCollector(
-        eval_env,
-        policy,
-    )
-    expl_path_collector = MdpPathCollector(
-        expl_env,
-        exploration_policy,
-    )
-    replay_buffer = EnvReplayBuffer(
-        variant['replay_buffer_size'],
-        expl_env,
-    )
-    trainer = TD3Trainer(
+    expl_path_collector = MdpPathCollector(expl_env, exploration_policy)
+    replay_buffer = EnvReplayBuffer(variant['replay_buffer_size'], expl_env)
+    trainer = DDPGTrainer(
+        qf=qf,
+        target_qf=target_qf,
         policy=policy,
-        qf1=qf1,
-        qf2=qf2,
-        target_qf1=target_qf1,
-        target_qf2=target_qf2,
         target_policy=target_policy,
         **variant['trainer_kwargs']
     )
@@ -131,22 +91,23 @@ def experiment(variant, args):
 
 
 if __name__ == "__main__":
-    pp = argparse.ArgumentParser()
-    pp.add_argument('--env', type=str)
-    args = pp.parse_args()
-
+    # noinspection PyTypeChecker
     variant = dict(
         algorithm_kwargs=dict(
-            num_epochs=3000,
-            num_eval_steps_per_epoch=5000,
+            num_epochs=1000,
+            num_eval_steps_per_epoch=1000,
             num_trains_per_train_loop=1000,
             num_expl_steps_per_train_loop=1000,
-            min_num_steps_before_training=1000,
+            min_num_steps_before_training=10000,
             max_path_length=1000,
-            batch_size=256,
+            batch_size=128,
         ),
         trainer_kwargs=dict(
+            use_soft_update=True,
+            tau=1e-2,
             discount=0.99,
+            qf_learning_rate=1e-3,
+            policy_learning_rate=1e-4,
         ),
         qf_kwargs=dict(
             hidden_sizes=[400, 300],
@@ -158,5 +119,5 @@ if __name__ == "__main__":
     )
     # By default GPU usage is True
     ptu.set_gpu_mode(True)
-    setup_logger('rlkit-td3-{}'.format(args.env), variant=variant)
-    experiment(variant, args)
+    setup_logger('rlkit-ddpg-{}'.format(args.env), variant=variant)
+    experiment(variant)
